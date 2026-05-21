b5a3646db280ed4952e3bcd8bd1c359aa3cb91f6
parthodhvani <parth.odhavani@iflair.co>
2026-05-21 13:03:07 +0530

    your message

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState, useRef } from "react";
 ══════════════════════════════════════════════════════════════ */
 // const BASE = "https://speller-choking-twisted.ngrok-free.dev/headless";
 
-const BASE = "https://wordpressvercel123.infinityfreeapp.com/headless/";
+const BASE = "https://wordpressvercel123.infinityfreeapp.com/headless";
 const PAGE_API    = `${BASE}/wp-json/wp/v2/pages/18`;
 const OPTIONS_API = `${BASE}/wp-json/custom/v1/options`;
 const MEDIA_API   = (id: number) => `${BASE}/wp-json/wp/v2/media/${id}`;
